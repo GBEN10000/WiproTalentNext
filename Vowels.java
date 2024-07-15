6fa2b3deea4bc98a74171b06cc7bbf77909805fd
Chintu_Gunturu <107250168+GBEN10000@users.noreply.github.com>
2024-07-15 14:00:36 +0530

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,16 @@
+class User {
+    public int countVowels(String input) {
+        int count = 0;
+        for (char c : input.toCharArray()) {
+            if (isVowel(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
