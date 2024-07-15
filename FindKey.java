051900c06bbc05359627c0ee3c745e965cadd939
Chintu_Gunturu <107250168+GBEN10000@users.noreply.github.com>
2024-07-15 13:58:27 +0530

    Create FindKey.java
    key= (1000*100) - smallestof3rdinput

---
+++ FindKey.java
@@ -0,0 +1,21 @@
+class User {
+    public int findKey(int inp1, int inp2, int inp3) {
+        int thousandDigit = (inp1 / 1000) % 10;
+        int hundredDigit = (inp2 / 100) % 10;
+        int largestDigit = findLargestDigit(inp3);
+        int key = (thousandDigit * hundredDigit) - largestDigit;
+        return key;
+    }
+
+    public int findLargestDigit(int number) {
+        int largest = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit > largest) {
+                largest = digit;
+            }
+            number /= 10;
+        }
+        return largest;
+    }
+}
